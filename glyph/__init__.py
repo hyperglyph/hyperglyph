fed316f9d97094d2ad8f4d911e943ab3915eb35e
tef <tef@twentygototen.org>
2012-03-17 10:37:24 +0000

    todo update

--- glyph/__init__.py
+++ glyph/__init__.py
@@ -1,6 +1,7 @@
 """ glyph-rpc - websites for robots """
 """
 Copyright (c) 2011-2012 Hanzo Archives Ltd
+Copyright (c) 2012 Thomas Figg
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
