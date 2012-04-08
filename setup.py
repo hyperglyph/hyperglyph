5ffc968a2f8d4e2e68973951ae35e6815132346b
tef <tef@twentygototen.org>
2012-04-08 07:09:53 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 from setuptools import setup
 
 setup(name='glyph-rpc',
-      version='0.1-20120317',
+      version='0.1-20120408',
       license="MIT License",
       description='glyph-rpc is well behaved rpc over http with callbacks',
       author='tef',
