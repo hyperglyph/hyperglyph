6516533d987ae1f693971224292b57c6f598170f
tef <tef@twentygototen.org>
2012-04-12 09:43:19 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ import glob
 from setuptools import setup
 
 setup(name='glyph-rpc',
-      version='0.1-20120409',
+      version='0.1-20120412',
       license="MIT License",
-      description='glyph-rpc is well behaved rpc over http with callbacks',
+      description='glyph-rpc is yet another http rpc library, but it tries to exploit http rather than simply tunnel requests over it.',
       author='tef',
       author_email='tef@twentygototen.org',
       packages=['glyph', 'glyph.resource'],
