0313b5769699f3043bfdba17803ffd388c6c218a
tef <thomas.figg@gmail.com>
2012-06-29 00:15:28 +0100

    nomenclature, documentation for hexfloat

--- rb/lib/hexfloat.rb
+++ rb/lib/hexfloat.rb
@@ -1,24 +1,33 @@
 # i am a terrible programmer and I should be ashamed.
-# todo nan, inf handling?
+
+# hex notation for floating point numbers
+# specified by c99, used in python, java
+# http://docs.oracle.com/javase/6/docs/api/java/lang/Float.html#toHexString(float)
+
+# floats are <sign bit> <exponent> <fractional>
+# normal hex floats are (-)0x1.<fractional>p<exponent>
+# 0.0 is 0x0.0p0, -0.0 is -0x0.0p0
+# subnormals are 0x0.<fractional>p-1022
+# infinity is -inf/infinity
 class String
   def hex_to_f
     s = self.downcase
     if s == 'nan'
       Float::NAN
-    elsif s.start_with? "inf"
+    elsif s == "inf" or s =='infinity'
       Float::INFINITY
-    elsif s.start_with? "-inf"
+    elsif s == "-inf" or s =='-infinity'
       -Float::INFINITY
     else
       m = /(-?)0x([01]).([0-9a-f]+)p(-?[0-9a-f]+)/.match s
 
       subnormal = (m[2] == "0")
-      mantissa = m[3].to_i(16)
+      fractional = m[3].to_i(16)
       exponent = m[4].to_i(16)
       sign =  m[1] == "-" ? 128 :0
 
       exponent = subnormal ? 0 : [(exponent+1023) <<4].pack('n').bytes.to_a
-      bits = [(mantissa >> 32), (mantissa & (2**32-1))].pack('NN').bytes.to_a
+      bits = [(fractional >> 32), (mantissa & (2**32-1))].pack('NN').bytes.to_a
       bits[0..1] = [ sign | exponent[0], exponent[1] | bits[1] ]
 
       bits.pack('C*').unpack('G')[0]
@@ -36,15 +45,15 @@ class Float
         exponent = (bits[0..1].pack('C*').unpack('n')[0] >> 4) - 1023
         bits[0]&=0
         bits[1]&=15
-        mantissa = bits.pack('C*').unpack('H*').join
-        mantissa.slice! /^0+(?=[1-9]|0$)/
+        fractional = bits.pack('C*').unpack('H*').join
+        fractional.slice! /^0+(?=[1-9]|0$)/
 
         if exponent > -1023
-          "#{sign}0x1.#{mantissa}p#{exponent.to_s(16)}"
-        elsif mantissa == "0"
+          "#{sign}0x1.#{fractional}p#{exponent.to_s(16)}"
+        elsif fractional == "0"
           "#{sign}0x0.0p0"
         else
-          "#{sign}0x0.#{mantissa}p-1022"
+          "#{sign}0x0.#{fractional}p-1022"
         end
       end
   end
